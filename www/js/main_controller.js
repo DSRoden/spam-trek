ad92d5b197af635c777a29828cb861d25e2299e7
DSRoden <Daniel.S.Roden@gmail.com>
2014-10-20 13:53:23 -0500

    Setup

---
+++ www/js/main_controller.js
@@ -0,0 +1,8 @@
+(function(){
+  'use strict';
+
+  angular.module('spam-trek')
+  .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
+  $scope.title = 'Spam-Trek';
+  }]);
+})();
